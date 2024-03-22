1e5851afc3a25cad456eaee49a33d2fb7d5a302a
inoyatovlee <inoyatovalijon02@gmail.com>
2024-03-22 23:19:20 +0500

    Row page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_widgets/widgets/ContainerPage.dart';
 import 'package:flutter_widgets/widgets/ImagePage.dart';
 import 'package:flutter_widgets/widgets/Padding.dart';
 import 'package:flutter_widgets/widgets/Rich.Text.dart';
+import 'package:flutter_widgets/widgets/RowPage.dart';
 import 'package:flutter_widgets/widgets/Text.rich.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ContainerPage(),
+      home: const RowPage(),
     );
   }
 }
