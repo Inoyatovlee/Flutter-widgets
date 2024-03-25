7b711a75231cdea494ea10f192f3b6f44ec257d2
inoyatovlee <inoyatovalijon02@gmail.com>
2024-03-25 16:15:11 +0500

    Row and Column

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_widgets/widgets/ImagePage.dart';
 import 'package:flutter_widgets/widgets/Padding.dart';
 import 'package:flutter_widgets/widgets/Rich.Text.dart';
 import 'package:flutter_widgets/widgets/RowPage.dart';
+import 'package:flutter_widgets/widgets/RowaColumn.dart';
 import 'package:flutter_widgets/widgets/Text.rich.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ColumnPage(),
+      home: const Row_Column(),
     );
   }
 }
